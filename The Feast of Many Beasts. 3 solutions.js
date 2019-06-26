f2e6cb03730a3bef8f6e88ed576305278edc7f1f
Liza_poliachenko <liyourlp@gmail.com>
2019-06-25 21:18:26 -0700

    3 different solutions to the same problem

--- The Feast of Many Beasts. 3 solutions.js
+++ The Feast of Many Beasts. 3 solutions.js
@@ -9,3 +9,24 @@ if (beast[beast.length - 1] !== dish[dish.length - 1]) {
 else
 return true;
 }
+
+-----------------
+
+function feast(beast, dish) {
+  if (beast[0] === dish[0] && beast[beast.length - 1] === dish[dish.length - 1]) {
+    return true;
+  }
+
+  return false;
+}
+ ----------------
+
+ function feast(beast, dish) {
+ /*  if (beast[0] === dish[0] && beast[beast.length - 1] === dish[dish.length - 1]) {
+     return true;
+   }
+
+   return false;*/
+
+   return beast[0] === dish[0] && beast[beast.length - 1] === dish[dish.length - 1];
+ }
