4ed65b58fbd77d9452ffdd4a0e3e2c947f631703
Liza_poliachenko <liyourlp@gmail.com>
2019-07-17 21:07:06 -0700

    Grasshopper - Summation

---
+++ Grasshopper.js
@@ -0,0 +1,11 @@
+/* https://www.codewars.com/kata/grasshopper-summation/train/javascript
+Grasshopper - Summation
+*/
+
+var summation = function (num) {
+ var sum = 0;
+ for(let i = 1; i <= num; i++) {
+ sum += i;
+ }
+ return sum;
+}
