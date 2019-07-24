db193071227608bb7cfa719413bf60cce6a03bd0
Liza_poliachenko <liyourlp@gmail.com>
2019-07-24 13:07:20 -0700

    how to reverse sting using loop for

---
+++ reversed string.js
@@ -0,0 +1,9 @@
+//https://www.codewars.com/kata/reversed-strings/train/javascript
+
+function solution(str){
+  var newString = "";
+    for (var i = str.length - 1; i >= 0; i--) {
+        newString += str[i];
+    }
+    return newString;
+}
