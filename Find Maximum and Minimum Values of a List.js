25bcab130998a822472d5cd8c11a358ca7a8734a
Liza_poliachenko <liyourlp@gmail.com>
2019-08-01 18:58:41 -0700

    cw solution

---
+++ Find Maximum and Minimum Values of a List.js
@@ -0,0 +1,14 @@
+/*
+https://www.codewars.com/kata/find-maximum-and-minimum-values-of-a-list/train/javascript
+Find Maximum and Minimum Values of a List
+
+*/
+
+const min = list => {
+
+    return Math.min(...list);
+}
+const max = list => {
+
+    return Math.max(...list);
+}
